b39501f52e595d2b0076ccc1b0e36fef96641217
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2026-01-17 20:52:21 +0530

    sortcolors

--- assignments/sortcolors.java
+++ assignments/sortcolors.java
@@ -33,7 +33,7 @@ public class sortcolors {
 
     public static void main(String[] args) {
 
-        int[] nums = {2, 0, 2, 1, 1, 0,1,2,3};
+        int[] nums = {2, 0, 2, 1, 1, 0,1,2,3,2,3,1,3,4,5};
 
         System.out.println("Before: " + Arrays.toString(nums));
 
